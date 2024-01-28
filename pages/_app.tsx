752fd6e04e7df2fa4160716468de8a533c26a429
Davidyehu <dyb613@gmail.com>
2024-01-27 20:51:12 -0500

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { LoadingContext } from "@/global-context";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useState } from "react";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <LoadingContext.Provider value={{loading, setLoading}}>
+      <Analytics />
       <Component {...pageProps} />
     </LoadingContext.Provider>
   );
